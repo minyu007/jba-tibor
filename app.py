b51bcafd8fb4f27aa2069078c36c04789ffb1b2a
Kevin <kevin@macbook.lan>
2024-04-17 21:38:53 +0800

    sync

--- app.py
+++ app.py
@@ -6,51 +6,55 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+try:
+    current_date = datetime.now().strftime("%y%m%d")
 
-current_date = datetime.now().strftime("%y%m%d")
+    base_url = "https://www.jbatibor.or.jp/rate/pdf/JAPANESEYENTIBOR{}.pdf"
 
-base_url = "https://www.jbatibor.or.jp/rate/pdf/JAPANESEYENTIBOR{}.pdf"
+    pdf_url = base_url.format(current_date)
 
-pdf_url = base_url.format(current_date)
+    tables = tabula.read_pdf(pdf_url, pages="all")
 
-tables = tabula.read_pdf(pdf_url, pages="all")
+    dfs = [pd.DataFrame(table) for table in tables]
 
-dfs = [pd.DataFrame(table) for table in tables]
+    df = pd.concat(dfs, ignore_index=True)
 
-df = pd.concat(dfs, ignore_index=True)
+    df.set_index(df.columns[0], inplace=True)
+    df.index.rename('date', inplace=True)
 
-df.set_index(df.columns[0], inplace=True)
-df.index.rename('date', inplace=True)
+    df.fillna(0, inplace=True)
 
+    first_row = df.iloc[0]
+    second_row = df.iloc[1]
 
-df.fillna(0, inplace=True)
+    change_columns = []
+    for column in df.columns:
+        if abs(first_row[column] - second_row[column]) > 0.001 * abs(first_row[column]):
+            change_columns.append(column)
 
-first_row = df.iloc[0]
-second_row = df.iloc[1]
+    mail_content = ""
+    if change_columns:
+        mail_content = " ".join(change_columns) + " changes more than 0.1%"
+        mail_content = "Hi, !\n\n" + mail_content + "\n\n Regards,\nRobot"
+        mail_content += "\n\nRefer to the link for more information:" + base_url
 
-change_columns = []
-for column in df.columns:
-    if abs(first_row[column] - second_row[column]) > 0.001 * abs(first_row[column]):
-        change_columns.append(column)
+    if mail_content:
+        sender_email = "chengguoyu_82@163.com"
+        receiver_emails = ["zling@jenseninvest.com", "13889632722@163.com"]
+        password = "SSJTQGALEZMNHNGE"
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = ", ".join(receiver_emails)
+        message["Subject"] = "Alert: Rate Changes"
+        message.attach(MIMEText(mail_content, "plain"))
+        with smtplib.SMTP("smtp.163.com", 25) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_emails, message.as_string())
+            print("Email sent successfully!")
+    else:
+        print("No significant changes to report.")
 
-mail_content = ""
-if change_columns:
-    mail_content = " ".join(change_columns) + " change more than 0.1%"
+    print(df)
 
-if mail_content:
-    sender_email = "chengguoyu_82@163.com"
-    receiver_email = "wo_oplove@163.com"
-    password = "SSJTQGALEZMNHNGE"
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = "Alert: Rate Changes"
-    message.attach(MIMEText(mail_content, "plain"))
-    with smtplib.SMTP("smtp.163.com", 25) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        print("Email sent successfully!")
-else:
-    print("No significant changes to report.")
-
-print(df)
+except Exception as e:
+    print("running into an error:", e)
--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 # 创建示例 DataFrame
 data = {
-    '1WEEK': [0.02, 0.03, 0.01],
-    '1MONTH': [0.05, 0.06, 0.07],
+    '1WEEK': [0.02, 0.02, 0.01],
+    '1MONTH': [0.06, 0.06, 0.07],
     '2MONTH': [None, None, None],
     '3MONTH': [0.08, 0.09, 0.1],
     '4MONTH': [None, None, None],
@@ -23,41 +23,39 @@ data = {
 dates = pd.date_range(start='2024-04-01', periods=3, freq='D')
 df = pd.DataFrame(data, index=dates)
 
-# 将缺失值替换为 0
 df.fillna(0, inplace=True)
 
 first_row = df.iloc[0]
 second_row = df.iloc[1]
 
-# 计算变化超过 0.1% 的列
 change_columns = []
 for column in df.columns:
     if abs(first_row[column] - second_row[column]) > 0.001 * abs(first_row[column]):
         change_columns.append(column)
 
-# 构造邮件内容
 mail_content = ""
 if change_columns:
     mail_content = " ".join(change_columns) + " change more than 0.1%"
+    mail_content = "Hi, recipient!\n\n" + \
+        mail_content + "\n\nBest regards,\nYour Sender"
+    mail_content += "\n\nClick the link for more information: http://baid.com"
 
-# 发送邮件
+# mail_content = ""
+# if change_columns:
+#     mail_content = " ".join(change_columns) + " change more than 0.1%"
+
 if mail_content:
     sender_email = "chengguoyu_82@163.com"
     receiver_email = "wo_oplove@163.com"
     password = "SSJTQGALEZMNHNGE"  # 你的邮箱密码
-
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = "Alert: Rate Changes"
-
     message.attach(MIMEText(mail_content, "plain"))
-
-    # 建立连接并登录到你的邮箱
     with smtplib.SMTP("smtp.163.com", 25) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
-
         print("Email sent successfully!")
 else:
     print("No significant changes to report.")
