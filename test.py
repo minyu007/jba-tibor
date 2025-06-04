5b8b9ba62a04a26ce59f4d94072e781c6028c01a
kevin <kevin.minyu@gmail.com>
2025-06-04 15:32:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
             
             # 解析本地 PDF
             # tables = tabula.read_pdf(filename, pages="all")
-            tables = tabula.read_pdf(filename, pages="all", stream=True) 
+            tables = tabula.read_pdf(filename, pages="all", lattice=True, multiple_tables=True)
             
             dfs = [pd.DataFrame(table) for table in tables]
             df = pd.concat(dfs, ignore_index=True)
