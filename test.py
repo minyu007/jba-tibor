54a2cf8174bc8e311ea49e62c0ff6d97514aa5a2
kevin <kevin.minyu@gmail.com>
2025-06-04 15:45:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         try:
             current_date = datetime.now().strftime("%y%m%d")
             pdf_url = f"https://www.jbatibor.or.jp/rate/pdf/JAPANESEYENTIBOR{current_date}.pdf"
-            pdf_url=f'https://www.jbatibor.or.jp/rate/pdf/JAPANESEYENTIBOR250530.pdf'
+            # pdf_url=f'https://www.jbatibor.or.jp/rate/pdf/JAPANESEYENTIBOR250530.pdf'
             filename = f"{current_date}.pdf"
             
             # 先下载文件
