a6b570cd31557b86fcce0e1d599bc9ecea7d9de1
kevin <kevin.minyu@gmail.com>
2025-10-06 11:42:11 +0800

    Update app6-test.py

--- app6-test.py
+++ app6-test.py
@@ -276,7 +276,7 @@ if __name__ == "__main__":
             
             
             df = pd.concat(dfs, ignore_index=True)
-            print(1, df) 
+            print(df) 
             
             df2 = pd.concat(dfs2, ignore_index=True)
 
