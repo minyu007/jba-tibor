d8def5e5abef1ca9f73a89fc488ceeae6dcb65c2
kevin <kevin.minyu@gmail.com>
2025-10-06 10:58:02 +0800

    Update app6-test.py

--- app6-test.py
+++ app6-test.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
                 multiple_tables=False,
                 stream=True, 
                 guess=False,
-                pandas_options={'header': 0}
+                pandas_options={'header': None}
             )
                         
             dfs = [pd.DataFrame(table) for table in tables]
