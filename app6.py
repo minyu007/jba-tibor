38a4a07d7cde33727a2db765c79ad135e4ed2831
kevin <kevin.minyu@gmail.com>
2025-10-07 01:09:22 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -206,7 +206,7 @@ def split_row_to_rows(df):
             split_data[col] = ['']
         else:
             # 分割字符串并去除空白字符
-            print(first_row[col])
+            # print(first_row[col])
             split_values = [v.strip() for v in first_row[col].split('\r')]
             split_data[col] = split_values
     
@@ -306,7 +306,7 @@ if __name__ == "__main__":
             df.set_index('Date', inplace=True)
             df.index.rename('date', inplace=True)
 
-            excel_path = './all_data.xlsx'
+            excel_path = './jba-tibor/all_data.xlsx'
             
             # 检查Excel文件是否存在
             if os.path.exists(excel_path):
