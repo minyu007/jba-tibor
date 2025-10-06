6130c2c17beba05a62ac44d2f4ccfc473f9a8006
kevin <kevin.minyu@gmail.com>
2025-10-06 10:57:16 +0800

    Update app6-test.py

--- app6-test.py
+++ app6-test.py
@@ -253,7 +253,7 @@ if __name__ == "__main__":
                 filename,
                 pages="all",
                 multiple_tables=False,
-                steam=True, 
+                stream=True, 
                 guess=False,
                 pandas_options={'header': 0}
             )
