0fd4acf3215b4c5b97834caa6e4b5bdc6f6f08c9
kevin <kevin.minyu@gmail.com>
2025-11-04 15:17:56 +0800

    Update app8.py

--- app8.py
+++ app8.py
@@ -258,6 +258,7 @@ if __name__ == "__main__":
             dfs = [pd.DataFrame(table) for table in tables]
             
             df = pd.concat(dfs, ignore_index=True)
+            df.insert(loc=0, column='Date', value=['2025-11-04'])
             # print(df.columns)
             # print(df)
             df.columns=['Date',
@@ -296,8 +297,8 @@ if __name__ == "__main__":
             
             sender_email = "chengguoyu_82@163.com"
             sender_password = "DUigKtCtMXw34MnB"
-            recipient_emails = ["zling@jenseninvest.com","hwang@jenseninvest.com", "yqguo@jenseninvest.com", "13889632722@163.com"]
-            # recipient_emails = ["wo_oplove@163.com"]
+            # recipient_emails = ["zling@jenseninvest.com","hwang@jenseninvest.com", "yqguo@jenseninvest.com", "13889632722@163.com"]
+            recipient_emails = ["wo_oplove@163.com"]
             subject = "Japanese Yen TIBOR"
             
             body = f"<p>Download PDF <a href='{pdf_url}' target='_blank'>click me!</a></p><br/><div>{html_table}</div><br/>"
